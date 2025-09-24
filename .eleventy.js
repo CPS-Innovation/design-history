df51f6bdfb902e5cc049e93ed21757165cd95acc
knowdigital <knowdigital@users.noreply.github.com>
2025-09-24 15:37:02 +0100

    has chatGPT got this right now :-)

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 module.exports = function (eleventyConfig) {
+  const pathPrefix = "/design-history"; // ✅ Set once here
+
   // Browser Sync
   eleventyConfig.setBrowserSyncConfig({
     rewriteRules: [{
@@ -9,34 +11,33 @@ module.exports = function (eleventyConfig) {
     serveStaticOptions: {
       extensions: ['html']
     }
-  })
+  });
+
+  // Expose pathPrefix globally so {{ pathPrefix }} works in templates
+  eleventyConfig.addGlobalData("pathPrefix", pathPrefix);
 
   // Template libraries
-  eleventyConfig.setLibrary('njk', require('./lib/libraries/nunjucks'))
-  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown'))
+  eleventyConfig.setLibrary('njk', require('./lib/libraries/nunjucks'));
+  eleventyConfig.setLibrary('md', require('./lib/libraries/markdown'));
 
   // Plugins
-  eleventyConfig.addPlugin(require('@11ty/eleventy-navigation'))
-  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'))
+  eleventyConfig.addPlugin(require('@11ty/eleventy-navigation'));
+  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'));
 
   // Filters
-  eleventyConfig.addFilter('date', require('./lib/filters/date'))
-  eleventyConfig.addFilter('markdown', require('./lib/filters/markdown'))
-  eleventyConfig.addFilter('pretty', require('./lib/filters/pretty'))
-  eleventyConfig.addFilter('tokenize', require('./lib/filters/tokenize'))
-  eleventyConfig.addFilter('widont', require('./lib/filters/widont'))
-
-  // Transforms
-
-  // Collections
+  eleventyConfig.addFilter('date', require('./lib/filters/date'));
+  eleventyConfig.addFilter('markdown', require('./lib/filters/markdown'));
+  eleventyConfig.addFilter('pretty', require('./lib/filters/pretty'));
+  eleventyConfig.addFilter('tokenize', require('./lib/filters/tokenize'));
+  eleventyConfig.addFilter('widont', require('./lib/filters/widont'));
 
   // Passthrough
-  eleventyConfig.addPassthroughCopy({ './app/images': '.' })
-  eleventyConfig.addPassthroughCopy({ 'node_modules/govuk-frontend/govuk/assets': 'assets' })
+  eleventyConfig.addPassthroughCopy({ './app/images': '.' });
+  eleventyConfig.addPassthroughCopy({ 'node_modules/govuk-frontend/govuk/assets': 'assets' });
 
   // Config
   return {
-    pathPrefix: "/design-history", // ✅ This is the only new line
+    pathPrefix, // ✅ Used here for Eleventy routing
     dataTemplateEngine: 'njk',
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
@@ -47,5 +48,5 @@ module.exports = function (eleventyConfig) {
       includes: '_components'
     },
     templateFormats: ['njk', 'md', '11ty.js']
-  }
-}
+  };
+};
